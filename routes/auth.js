e9735106bbd2b1f6feed2ad6e832d94f2c9665f2
lukslda <lukslda@gmail.com>
2020-02-20 21:01:19 -0300

    error

--- routes/auth.js
+++ routes/auth.js
@@ -9,10 +9,6 @@ const auth = require('../middleware/auth');
 // Iniciar sesion
 // api/auth
 router.post('/',
-    [
-      check('email','Agrega un email valido').isEmail(),
-      check('password', 'El password debe ser minimo de 6 caracteres').isLength({min:6}) 
-    ],
     authController.autenticarUsuario
 );
 
